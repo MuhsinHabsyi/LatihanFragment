f11dd6fd7ce2230cd282b9be3ad239110b220ab1
Muhsin Askari <70380560+MuhsinHabsyi@users.noreply.github.com>
2021-11-25 16:37:22 +0700

    Initial commit

---
+++ app/src/main/java/id/luthfisolahudin/smkn4/pwpb/intent/latihanfragment/ui/home/HomeFragment.java
@@ -0,0 +1,21 @@
+package id.luthfisolahudin.smkn4.pwpb.intent.latihanfragment.ui.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.luthfisolahudin.smkn4.pwpb.intent.latihanfragment.R;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
